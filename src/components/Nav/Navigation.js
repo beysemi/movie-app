910fa7eeeb2351cac349b0e0f4941ad18f4da627
Beysemi Emirhan BAYRAK <beysemiemirhanbayrak@gmail.com>
2022-06-11 21:18:24 +0300

    folder update

--- src/components/Nav/Navigation.js
+++ src/components/Nav/Navigation.js
@@ -33,3 +33,4 @@ const Navigation = () => {
         </NavigationContainer>
     )
 }
+export default Navigation;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../components/Layout";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Navigation from "../components/Nav";
 export default function Home() {
   const Meta = {
     title: "Home",
@@ -13,6 +14,7 @@ export default function Home() {
       <Layout title={Meta.title} description={Meta.description} image={Meta.image}>
         <Header/>
         <Hero/>
+        <Navigation/>
       </Layout>
     </>
   )
